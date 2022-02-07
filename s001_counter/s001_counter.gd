9bcfa2a37ca5956fbfc7d1331dacb99e4a5d6df4
jiro4989 <jiroron666@gmail.com>
2022-02-07 21:23:51 +0900

    :tada: init commit

---
+++ s001_counter/s001_counter.gd
@@ -0,0 +1,18 @@
+extends Button
+
+
+func _init():
+	print("hello world")
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func _on_Button_pressed():
+	$"/root/Global_s001".counter += 1
