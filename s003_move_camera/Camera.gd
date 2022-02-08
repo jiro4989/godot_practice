e53c9e1c660b0045aeb7d46053fb3af3e242af36
jiro4989 <jiroron666@gmail.com>
2022-02-08 20:59:52 +0900

    カメラを動かすサンプルを追加

---
+++ s003_move_camera/Camera.gd
@@ -0,0 +1,9 @@
+extends Camera2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+func _process(delta):
+	offset = get_viewport().get_mouse_position()
