2b831b62cc0648c5b3f011f1dcbcdf5de4c4b474
jiro4989 <jiroron666@gmail.com>
2022-04-03 20:28:54 +0900

    wip

--- src/s005_enemy_ui/enemy.gd
+++ src/s005_enemy_ui/enemy.gd
@@ -1,11 +1,13 @@
 extends Node2D
 
+signal add_damage
+
 export (String) var enemy_name
 export (int) var mhp
 var hp = 0
 
-onready var name_label = $name_label
-onready var hp_label = $hp_label
+onready var name_label = $NameLabel
+onready var hp_label = $HPLabel
 
 func _ready():
 	hp = mhp
@@ -20,3 +22,8 @@ func add_damage(damage):
 	if point < 0:
 		point = 0
 	hp = point
+
+
+func _on_Node2D_add_damage():
+	print("got damage")
+	add_damage(5)
---
+++ src/s005_enemy_ui/icon.gd
@@ -0,0 +1,16 @@
+extends Sprite
+
+
+func _ready():
+	pass
+
+func _input(event):
+	#print("Input")
+	if event is InputEventMouseButton \
+	and event.button_index == BUTTON_LEFT \
+	and event.is_pressed():
+		self.on_click()
+
+func on_click():
+	#print("Click")
+	emit_signal("add_damage")
